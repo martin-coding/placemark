bde758fffcdccc640c5533ad4e0ea370013bffeb
martin-coding <89018576+martin-coding@users.noreply.github.com>
2025-12-04 21:10:39 +0100

    docs(api): add Swagger documentation for API

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,28 +1,47 @@
 import Joi from "joi";
 
-export const UserSpec = {
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-};
+export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
 
-export const UserCredentialsSpec = {
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-};
+export const UserCredentialsSpec = Joi.object()
+  .keys({
+    email: Joi.string().email().example("homer@simpson.com").required(),
+    password: Joi.string().example("secret").required(),
+  })
+  .label("UserCredentials");
 
-export const LocationSpec = {
-  title: Joi.string().required(),
-  latitude: Joi.number().required().min(-90).max(90).messages({
-    "number.base": "Latitude must be a valid number.",
-    "number.min": "Latitude must be between -90 and 90.",
-    "number.max": "Latitude must be between -90 and 90.",
-  }),
-  longitude: Joi.number().required().min(-180).max(180).messages({
-    "number.base": "Longitude must be a valid number.",
-    "number.min": "Longitude must be between -180 and 180.",
-    "number.max": "Longitude must be between -180 and 180.",
-  }),
-  description: Joi.string().allow("").optional(),
-};
+export const UserSpec = UserCredentialsSpec.keys({
+  firstName: Joi.string().example("Homer").required(),
+  lastName: Joi.string().example("Simpson").required(),
+}).label("UserDetails");
+
+export const UserSpecPlus = UserSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("UserDetailsPlus");
+
+export const UserArray = Joi.array().items(UserSpecPlus).label("UserArray");
+
+export const LocationSpec = Joi.object()
+  .keys({
+    title: Joi.string().required().example("Seljalandsfoss"),
+    description: Joi.string().allow("").optional(),
+    latitude: Joi.number().required().min(-90).max(90).messages({
+      "number.base": "Latitude must be a valid number.",
+      "number.min": "Latitude must be between -90 and 90.",
+      "number.max": "Latitude must be between -90 and 90.",
+    }),
+    longitude: Joi.number().required().min(-180).max(180).messages({
+      "number.base": "Longitude must be a valid number.",
+      "number.min": "Longitude must be between -180 and 180.",
+      "number.max": "Longitude must be between -180 and 180.",
+    }),
+    userid: IdSpec,
+  })
+  .label("Location");
+
+export const LocationSpecPlus = LocationSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("LocationPlus");
+
+export const LocationArraySpec = Joi.array().items(LocationSpecPlus).label("LocationArray");
