2e61e07578a9d0f9cebfadd55dc703ea1fa862b2
martin-coding <89018576+martin-coding@users.noreply.github.com>
2026-01-16 22:20:05 +0100

    feat: finalize location crud test

--- cypress/e2e/locations.cy.js
+++ cypress/e2e/locations.cy.js
@@ -5,30 +5,32 @@ describe("Location CRUD", () => {
   let userId;
 
   before(() => {
-    cy.fixture("location").then((l) => {
-      location = l;
-    });
-    cy.fixture("user").then((u) => {
-      user = {
-        ...u,
-        email: `test_${Date.now()}@example.com`,
-      };
-      cy.request("POST", "/api/users", {
-        email: user.email,
-        password: user.password,
-        firstName: user.firstName,
-        lastName: user.lastName,
-      }).then((response) => {
+    cy.fixture("user")
+      .then((u) => {
+        user = { ...u, email: `test_${Date.now()}@example.com` };
+
+        return cy.request("POST", "/api/users", {
+          email: user.email,
+          password: user.password,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        });
+      })
+      .then((response) => {
         expect(response.status).to.eq(201);
         userId = response.body._id;
-      });
-      cy.request("POST", "/api/users/authenticate", {
-        email: user.email,
-        password: user.password,
-      }).then((response) => {
+        return cy.request("POST", "/api/users/authenticate", {
+          email: user.email,
+          password: user.password,
+        });
+      })
+      .then((response) => {
         expect(response.status).to.eq(201);
         authToken = response.body.token;
       });
+
+    cy.fixture("location").then((l) => {
+      location = l;
     });
   });
 
@@ -37,6 +39,8 @@ describe("Location CRUD", () => {
       cy.request("POST", "/authenticate", {
         email: user.email,
         password: user.password,
+      }).then(() => {
+        cy.getCookie("placemark").should("exist");
       });
     });
   });
@@ -53,7 +57,7 @@ describe("Location CRUD", () => {
     cy.get("form").submit();
   });
 
-  it("reads a location", () => {
+  it("reads, updates and deletes a location", () => {
     location.userid = userId;
     cy.request({
       method: "POST",
@@ -70,11 +74,39 @@ describe("Location CRUD", () => {
       // Read
       cy.visit(`/location/${locationId}`);
 
+      cy.contains("h1", location.title);
+      cy.get("p.subtitle").should("contain.text", location.category).and("contain.text", location.visibility);
+      cy.contains(location.latitude);
+      cy.contains(location.longitude);
+      cy.contains(location.description);
+
       // Update
       cy.get("[data-cy=\"edit-location\"]").click();
+      cy.url().then((url) => {
+        const params = new URL(url).searchParams;
+        const edit = params.get("edit");
+        expect(edit).to.eq("true");
+      });
+
+      cy.get("input[name=title]").clear().type("Black sand");
+      cy.get("select[name=\"category\"]").select("beach");
+      cy.get("select[name=\"visibility\"]").select("private");
+      cy.get("input[name=\"latitude\"]").clear().type(64);
+      cy.get("input[name=\"longitude\"]").clear().type(-19);
+      cy.get("textarea[name=description]").clear().type("I was there");
+      cy.get("#edit-location-form").submit();
+
+      cy.contains("h1", "Black sand");
+      cy.get("p.subtitle").should("contain.text", "beach").and("contain.text", "private");
+      cy.contains(64);
+      cy.contains(-19);
+      cy.contains("I was there");
 
       // Delete
       cy.get("[data-cy=\"delete-location\"]").click();
+      cy.url().should("include", "/dashboard");
+      cy.visit(`/location/${locationId}`, { failOnStatusCode: false });
+      cy.contains("Page not found");
     });
   });
 });
